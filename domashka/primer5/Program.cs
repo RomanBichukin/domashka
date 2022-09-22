a2e8cecd67fb4dbae57b55455db5fa94f4e73aea
Roman_B <roman.bichukin@gmail.com>
2022-09-22 17:00:04 +0900

    hgf

--- domashka/primer5/Program.cs
+++ domashka/primer5/Program.cs
@@ -3,3 +3,9 @@
 //  456 -> 6
 //  782 -> 2
 //  918 -> 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int result = A % 10;
+Console.Write($"Результат: {result}");
