e644092585e789e2488f24b62bbe4fd432305944
Roman_B <roman.bichukin@gmail.com>
2022-09-29 15:34:12 +0900

    добавил строчку

--- domashka/seminar__3/Dz__3sem/D_z_2/Program.cs
+++ domashka/seminar__3/Dz__3sem/D_z_2/Program.cs
@@ -8,5 +8,5 @@ Console.WriteLine("Введите координаты первой точки: 
 int[] X = new int[3] { Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), };
 Console.WriteLine("Введите координаты первой точки: ");
 int[] Y = new int[3] { Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), };
-
+Console.Write($"X ({X[0]},{X[1]},{X[2]}); Y ({Y[0]}, {Y[1]}, {Y[2]}) -> ");
 Console.WriteLine(Math.Sqrt((Math.Pow((Y[0] - X[0]), 2)) + (Math.Pow((Y[1] - X[1]), 2)) + (Math.Pow((Y[2] - X[2]), 2))));
