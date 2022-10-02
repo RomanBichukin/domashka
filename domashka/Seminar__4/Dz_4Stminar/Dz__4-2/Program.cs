820f3d312af2179ea3aed200538c9a70bcffc064
Roman_B <roman.bichukin@gmail.com>
2022-10-02 12:21:34 +0900

    Дз___семинар4

---
+++ domashka/Seminar__4/Dz_4Stminar/Dz__4-2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//      452 -> 11
+//      82 -> 10
+//      9012 -> 12
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (A > 0)
+{
+    result = result + A%10;
+    A = A / 10;
+    
+}
+Console.WriteLine(result);
