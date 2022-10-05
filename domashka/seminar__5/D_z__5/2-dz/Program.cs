f228dd16005fa7ce1c9fef8a24613e49cc4ef45d
Roman_B <roman.bichukin@gmail.com>
2022-10-05 19:46:35 +0900

    Дз___семинар5

---
+++ domashka/seminar__5/D_z__5/2-dz/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//      [3, 7, 23, 12] -> 19
+//      [-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] array = new int[4];
+Random rnd = new Random();
+int Sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-100, 100);
+    Console.Write($"{array[i]}, ");
+    if (i%2 != 0)
+    {
+        Sum = Sum + array[i];
+
+    }
+}
+Console.Write($"сумма нечётных позиций -> {Sum}!!!");
---
+++ domashka/seminar__5/D_z__5/3-dz/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+//      [3 7 22 2 78] -> 76
+
+
+Console.Clear();
+
+double[] array = new double[10];
+Random rnd = new Random();
+//int max = array[i];
+//int min = array[i];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.NextDouble() * 100;
+    Console.WriteLine("{0,3:F2}", array[i]);//"{0,3}:F2" - ограничиваем кол-вр знаков после запятой!!!
+}
+
+/* for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(100);
+    Console.Write("{0,3:F2}",array[i] + "  ");
+} */
+
+/* void PrintArray(double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.NextDouble() * 100;
+        Console.Write("{0,3:F2}",array[i] + " ");
+    }
+    //Console.WriteLine();
+}
+PrintArray(array);
+ */
+
+
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"всего {array.Length} чисел. ");
+Console.WriteLine($"Максимальное значение = {max} ");
+Console.WriteLine($"минимальное значение = {min} ");
+Console.WriteLine($"Разница между max и min значением = {max - min}");
