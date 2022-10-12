2be324a2ed0b2b0d46ae8cf29e1244beefc61160
Roman_B <roman.bichukin@gmail.com>
2022-10-12 11:34:12 +0900

    _семинар_6

--- domashka/Seminar__6/D_z__6/Program.cs
+++ domashka/Seminar__6/D_z__6/Program.cs
@@ -4,7 +4,7 @@
 //      1, -7, 567, 89, 223-> 3
 Console.Clear();
 
-/*  Console.Write("Введите колличество чисел: ");
+ Console.Write("Введите колличество чисел: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 for (int i = 0; i < n; i++)
@@ -14,7 +14,7 @@ for (int i = 0; i < n; i++)
     if (x > 0)
         count++;
 }
-Console.WriteLine($"Положительных чисел: {count}"); */
+Console.WriteLine($"Положительных чисел: {count}");
 //==================================================================================================
 //==================================================================================================
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
@@ -22,7 +22,7 @@ Console.WriteLine($"Положительных чисел: {count}"); */
 // значения b1, k1, b2 и k2 задаются пользователем.
 //  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите k1: ");
+/* Console.Write("Введите k1: ");
 int k1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите b1: ");
 int b1 = Convert.ToInt32(Console.ReadLine());
@@ -36,4 +36,4 @@ int b2 = Convert.ToInt32(Console.ReadLine());
  //
 int x = (b2-b1) / (k1 - k2);
 int y = k1 * x + b1;
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+Console.WriteLine($"Пересечение в точке: ({x};{y})"); */
--- domashka/Seminar__6/lesson__6/Program.cs
+++ domashka/Seminar__6/lesson__6/Program.cs
@@ -72,9 +72,9 @@ for (int i = 1; i < 40; i++)
 //Числа ФИБОНАЧИ: каждое последующее, равно сумме двух предыдущих...
 //  f(1) = 1
 //  f(2) = 1
-//  f(n) = f(n-1) + f(n-2)
+//  f(n) = f(n-1) + f(n-2)_____!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-int Fibonacci(int n)
+/* int Fibonacci(int n)
 {
     if(n == 1 || n == 2) return 1;
     else return Fibonacci(n-1) + Fibonacci(n-2);
@@ -82,4 +82,125 @@ int Fibonacci(int n)
 for (int i = 1; i < 50; i++)
 {
     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+} */
+
+
+//======================================================================================================
+//=================СЕМИНАР__6===========================================================================
+//======================================================================================================
+//**Задача 39:** Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//      [1 2 3 4 5] -> [5 4 3 2 1]
+//      [6 7 3 6] -> [6 3 7 6]
+/*     // создаёт рандомный массив!!!!
+int[] RandomArrayInt(int elements, int min, int max)
+{
+    int[] array = new int[elements];
+    for(int i = 0; i < elements; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
 }
+    //метод возвращает строку из массива int
+string ArrayIntToString(int[] arr)
+{
+    string result = "[";
+    for(int i = 0; i < arr.Length; i++)
+    if(i < arr.Length - 1)
+    {
+        result = result + $"{arr[i]}, ";
+    }
+    else
+    {
+        result = result + $"{arr[i]}";
+    }
+    result = result +"]";
+    return result;
+}
+int[] array = RandomArrayInt(10, 100, 1000);
+int[] newArray = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    newArray[i] = array[array.Length - i -1];
+}
+
+Console.Write($"{ArrayIntToString(array)} -> {ArrayIntToString(newArray)}"); */
+//=====================================================================================================
+//**Задача 40:** Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длины.
+//❗ **Теорема о неравенстве треугольника:** каждая сторона треугольника меньше суммы двух других сторон.
+
+/* Console.WriteLine("Введите первую сторону треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третью сторону треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a<b+c && b<a+c && c<a+b)
+{
+    Console.WriteLine("Треугольник с такими параметрами существует!!");
+}
+else
+{
+    Console.WriteLine("Такоо треугольника не существует!!");
+} */
+//=================================================================================================
+//**Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//  45 -> 101101
+//  3 -> 11
+//  2 -> 10
+
+/* Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string x = string.Empty;
+while(a != 0)
+{
+    x = Convert.ToString(a % 2) + x;///записываем остаток от деления
+    a = a /2;
+}
+Console.WriteLine($"{x}"); */
+
+
+//=================================================================================================
+//**Задача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+//  Если N = 5 -> 0 1 1 2 3
+//  Если N = 3 -> 0 1 1
+//  Если N = 7 -> 0 1 1 2 3 5 8
+  //метод возвращает строку из массива int
+string ArrayIntToString(int[] arr)
+{
+    string result = "[";
+    for(int i = 0; i < arr.Length; i++)
+    if(i < arr.Length - 1)
+    {
+        result = result + $"{arr[i]}, ";
+    }
+    else
+    {
+        result = result + $"{arr[i]}";
+    }
+    result = result + "]";
+    return result;
+}
+Console.WriteLine($"Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] f = new int[a];
+f[0] = 0;
+f[1] = 1;
+for(int i = 2; i < a; i++)
+{
+    f[i] = f[i - 1] + f[i-2];
+}
+Console.WriteLine($"{a} -> {ArrayIntToString(f)}");
+
+/* Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ */
+
+//=================================================================================================
+//Задача 45: Напишите программу, которая будет создавать копию заданного массива
+// с помощью поэлементного копирования.
